5403cc01b0ff100855fbc65dd4e57efa87a3e40a
LuCkEr- <robloxraido11@gmail.com>
2017-01-22 20:59:30 +0200

    Login page is done

--- web/access-control-kuristiku/resources/views/auth/login.blade.php
+++ web/access-control-kuristiku/resources/views/auth/login.blade.php
@@ -1,37 +1,13 @@
 @extends('layouts.authenticate')
-<!--
-<form role="form">
-<fieldset>
-    <div class="form-group">
-        <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
-    </div>
-    <div class="form-group">
-        <input class="form-control" placeholder="Password" name="password" type="password" value="">
-    </div>
-    <div class="checkbox">
-        <label>
-            <input name="remember" type="checkbox" value="Remember Me">Remember Me
-        </label>
-    </div>
-    -->
-    <!-- Change this to a button or input when using this as a form -->
-    <!--
-    <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
-</fieldset>
-</form>
--->
 
 @section('authTitle', 'Login')
 
 @section('authBody')
-<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+<form class="form" role="form" method="POST" action="{{ url('/login') }}">
 {{ csrf_field() }}
 
-    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-        <div class="col-md-6">
-            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="E-mail" required autofocus>
 
             @if ($errors->has('email'))
                 <span class="help-block">
@@ -39,13 +15,9 @@
                 </span>
             @endif
         </div>
-    </div>
 
-    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-        <label for="password" class="col-md-4 control-label">Password</label>
-
-        <div class="col-md-6">
-            <input id="password" type="password" class="form-control" name="password" required>
+        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+            <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
 
             @if ($errors->has('password'))
                 <span class="help-block">
@@ -53,28 +25,21 @@
                 </span>
             @endif
         </div>
-    </div>
 
-    <div class="form-group">
-        <div class="col-md-6 col-md-offset-4">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
-                </label>
-            </div>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
+            </label>
         </div>
-    </div>
 
-    <div class="form-group">
-        <div class="col-md-8 col-md-offset-4">
-            <button type="submit" class="btn btn-primary">
+        <div class="form-group">
+            <button type="submit" class="btn btn-lg btn-success btn-block">
                 Login
             </button>
-
-            <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                Forgot Your Password?
-            </a>
         </div>
-    </div>
+
+        <a class="btn btn-link btn-block" href="{{ url('/password/reset') }}">
+            Forgot Your Password?
+        </a>
 </form>
 @endsection
