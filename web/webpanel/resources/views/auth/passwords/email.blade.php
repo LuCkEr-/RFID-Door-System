b95a559fdd8cb159ce61ae8f2d6d752273e1371b
LuCkEr- <robloxraido11@gmail.com>
2017-02-05 20:41:41 +0200

    Added panel pages and components

--- web/webpanel/resources/views/auth/passwords/email.blade.php
+++ web/webpanel/resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,35 @@
-@extends('layouts.app')
+@extends('layouts.authenticate')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('authTitle', 'Reset Password')
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+@section('authBody')
+<div class="panel-body" style="padding-bottom: 0px;">
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
         </div>
-    </div>
+    @endif
+
+    <form class="form" role="form" method="POST" action="{{ url('/password/email') }}">
+        <fieldset>
+        {{ csrf_field() }}
+
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <input id="email" type="email" class="form-control" name="email" placeholder="E-Mail" value="{{ old('email') }}" required>
+
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block">
+                    Send Password Reset Link
+                </button>
+            </div>
+        <fieldset>
+    </form>
 </div>
 @endsection
---
+++ web/webpanel/resources/views/partials/sidebar.blade.php
@@ -0,0 +1,9 @@
+<li>
+	<a href="{{ url('/') }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+	<a href="{{ url('/accounts') }}"><i class="fa fa-users fa-fw"></i> Accounts</a>
+</li>
+<li>
+	<a href="{{ url('/cards') }}"><i class="fa fa-credit-card fa-fw"></i> Cards</a>
+</li>
