a11cb6876310f5786b5f8b5da9bb2f237d8cab77
LuCkEr- <robloxraido11@gmail.com>
2017-02-12 19:48:20 +0200

    Formating

--- src/KuristikuAccessControl.ino
+++ src/KuristikuAccessControl.ino
@@ -1,330 +1,304 @@
 /*
-Typical pin layout used:
---------------------------------
-MFRC522      Arduino
-Reader/PCD   Mega
-Signal      Pin          Pin
---------------------------------
-RST/Reset   RST          Custom
-SPI SS 1    SDA(SS)      Custom
-SPI SS 2    SDA(SS)      Custom
-SPI MOSI    MOSI         51
-SPI MISO    MISO         50
-SPI SCK     SCK          52
+   Typical pin layout used:
+   --------------------------------
+               MFRC522      Arduino
+               Reader/PCD   Mega
+   Signal      Pin          Pin
+   --------------------------------
+   RST/Reset   RST          Custom
+   SPI SS 1    SDA(SS)      Custom
+   SPI SS 2    SDA(SS)      Custom
+   SPI MOSI    MOSI         51
+   SPI MISO    MISO         50
+   SPI SCK     SCK          52
 */
 
 //==========//
 // INCLUDES //
 //==========//
-//---------//
-// ARDUINO //
-//---------//
-#include <SPI.h>
+  //---------//
+  // ARDUINO //
+  //---------//
+  #include <SPI.h>
 
-//------//
-// RFID //
-//------//
-#include <MFRC522.h>
+  //------//
+  // RFID //
+  //------//
+  #include <MFRC522.h>
 
-//----------//
-// ETHERNET //
-//----------//
-#include <Ethernet2.h>
+  //----------//
+  // ETHERNET //
+  //----------//
+  #include <Ethernet2.h>
 
-//-------//
-// MYSQL //
-//-------//
-#include <MySQL_Connection.h>
-//#include <MySQL_Cursor.h>
+  //-------//
+  // MYSQL //
+  //-------//
+  #include <MySQL_Connection.h>
+  //#include <MySQL_Cursor.h>
 
 //=========//
 // DEFINES //
 //=========//
-//------//
-// RFID //
-//------//
-// RST/Reset | PIN
-#define RST_PIN_1       5
-#define RST_PIN_2       6
+  //------//
+  // RFID //
+  //------//
+  // RST/Reset | PIN
+  #define RST_PIN       2
 
-// SDA       | PIN
-#define SDA_PIN_1       40
-#define SDA_PIN_2       53
+  // SDA       | PIN
+  #define SDA_PIN_1     40
+  #define SDA_PIN_2     41
 
-// Number of readers
-#define READER_TOTAL    2
+  // Number of readers
+  #define READER_TOTAL  1
 
-//-----//
-// LED //
-//-----//
-#define LED_BLUE_PIN    7
-#define LED_YELLOW_PIN  8
-#define LED_RED_PIN     12
+  //-----//
+  // LED //
+  //-----//
+  #define LED_BLUE_PIN 7
+  #define LED_YELLOW_PIN 8
+  #define LED_RED_PIN 12
 
 //===========//
 // INSTANCES //
 //===========//
-//------//
-// RFID //
-//------//
-MFRC522 mfrc522[READER_TOTAL];
+  //------//
+  // RFID //
+  //------//
+  MFRC522 mfrc522[READER_TOTAL];
 
-//----------//
-// ETHERNET //
-//----------//
-EthernetClient ethernetClient;
+  //----------//
+  // ETHERNET //
+  //----------//
+  EthernetClient ethernetClient;
 
-//-------//
-// MYSQL //
-//-------//
-MySQL_Connection mysqlClient((Client *)&ethernetClient);
-//MySQL_Cursor mysqlPos = MySQL_Cursor(&mysqlClient);
+  //-------//
+  // MYSQL //
+  //-------//
+  MySQL_Connection mysqlClient((Client *)&ethernetClient);
+  //MySQL_Cursor mysqlPos = MySQL_Cursor(&mysqlClient);
 
 //==================//
 // GLOBAL VARIABLES //
 //==================//
-//------//
-// RFID //
-//------//
-byte rstPins[] = {RST_PIN_1, RST_PIN_2};
-byte sdaPins[] = {SDA_PIN_1, SDA_PIN_2};
+  //------//
+  // RFID //
+  //------//
+  byte sdaPins[] = {SDA_PIN_1, SDA_PIN_2};
 
-//----------//
-// ETHERNET //
-//----------//
-// Mac address
-byte eth_mac[] = { 0xAD, 0x07, 0xF0, 0x42, 0x00, 0x69 };
+  //----------//
+  // ETHERNET //
+  //----------//
+  // Mac address
+  byte eth_mac[] = { 0xAD, 0x07, 0xF0, 0x42, 0x00, 0x69 };
 
-// IP address
-byte eth_IP[] = {192, 168, 10, 90};
+  // IP address
+  byte eth_IP[] = {192, 168, 10, 90};
 
-// Ethernet connected
-bool isEthConnected = false;
+  //-------//
+  // MYSQL //
+  //-------//
+  // Mysql server ip
+  IPAddress mysql_IP(192, 168, 10, 2);
 
-//-------//
-// MYSQL //
-//-------//
-// Mysql server ip
-IPAddress mysql_IP(192, 168, 10, 2);
+  // Mysql server port
+  int mysql_Port = 3066;
 
-// Mysql server port
-int mysql_Port = 3066;
+  // Mysql user
+  char mysql_user[] = "kuristiku";
 
-// Mysql user
-char mysql_user[] = "kuristiku";
+  // Mysql password
+  char mysql_password[] = "eFo04ppjNkG3JJOD";
 
-// Mysql password
-char mysql_password[] = "eFo04ppjNkG3JJOD";
-
-//----------------//
-// TESTING VALUES //
-//----------------//
-int TotalLoadedCards = 10;
+  //----------------//
+  // TESTING VALUES //
+  //----------------//
+  int TotalLoadedCards = 10;
 
 //=======//
 // SETUP //
 //=======//
 void setup() {
-    // Init serial communications with the PC
-    Serial.begin(115200);
+  // Init serial communications with the PC
+  Serial.begin(115200);
 
-    // Wait while the serial port is being opened
-    while (!Serial);
-    Serial.println();
-    Serial.println("Starting...");
+  // Wait while the serial port is being opened
+  while (!Serial);
+  Serial.println();
+  Serial.println("Starting...");
 
-    // Init LED PINS
-    init_LED();
+  // Init LED PINS
+  init_LED();
 
-    // Init Ethernet
-    //init_Ethernet();
+  // Init Ethernet
+  init_Ethernet();
 
-    // Init MYSQL
-    init_Mysql();
+  // Init MYSQL
+  init_Mysql();
 
-    // Init SPI bus
-    Serial.print("Init SPI...");
-    SPI.begin();
-    Serial.println(" [OK]");
+  // Init SPI bus
+  Serial.print("Init SPI...");
+  SPI.begin();
+  Serial.println(" [OK]");
 
-    // Init Rfid readers
-    init_Rfid();
+  // Init Rfid readers
+  init_Rfid();
 
-    Serial.println("Startup finished");
+  Serial.println("Startup finished");
 }
 
 //=======//
 // Inits //
 //=======//
 void init_LED() {
-    Serial.print("Assigning LED outputs...");
-    pinMode(LED_BLUE_PIN, OUTPUT);
-    pinMode(LED_YELLOW_PIN, OUTPUT);
-    pinMode(LED_RED_PIN, OUTPUT);
-    Serial.println(" [OK]");
+  Serial.print("Assigning LED outputs...");
+  pinMode(LED_BLUE_PIN, OUTPUT);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
+  Serial.println(" [OK]");
 }
 
 void init_Ethernet() {
-    Serial.print("Init Ethernet...");
+  Serial.print("Init Ethernet...");
 
-    if (Ethernet.begin(eth_mac) != 0) {
-        // Connection was made
-        Serial.println(" [OK]");
-        isEthConnected = true;
-    } else {
-        // No connection
-        Serial.println(" [FAIL]");
-        isEthConnected = false;
-    }
+  if (Ethernet.begin(eth_mac) != 0) {
+    // Connection was made
+    Serial.println(" [OK]");
+  } else {
+    // No connection
+    Serial.println(" [FAIL]");
+  }
 
-    if (isEthConnected) {
-        // Print IP address
-        Serial.print("IP Address: ");
-        for (byte thisByte = 0; thisByte < 4; thisByte++) {
-            // print the value of each byte of the IP address:
-            Serial.print(Ethernet.localIP()[thisByte], DEC);
-            Serial.print(".");
-        }
-        Serial.println();
-    }
+  // Print IP address
+  Serial.print("IP Address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
 }
 
 void init_Mysql () {
-    Serial.print("Init Mysql...");
+  Serial.print("Init Mysql...");
 
-    if (isEthConnected) {
-        if (mysqlClient.connect(mysql_IP, mysql_Port, mysql_user, mysql_password)) {
-            delay(1000);
-            // Connection was made
-            Serial.println(" [OK]");
-        } else {
-            // No connection
-            Serial.println(" [FAIL]");
-            mysqlClient.close();
-        }
-    } else {
-        Serial.println(" [FAIL]");
-    }
+  if (mysqlClient.connect(mysql_IP, mysql_Port, mysql_user, mysql_password)) {
+    delay(1000);
+    // Connection was made
+    Serial.println(" [OK]");
+  } else {
+    // No connection
+    Serial.println(" [FAIL]");
+    mysqlClient.close();
+  }
 }
 
 void init_Rfid() {
-    for (uint8_t reader = 0; reader < READER_TOTAL; reader++) {
-        // buffer
-        char buffer[32];
+  for (uint8_t reader = 0; reader < READER_TOTAL; reader++) {
+    // buffer
+    char buffer[32];
 
-        // Init reader
-        sprintf(buffer, "%i | Init reader...", reader);
-        Serial.print(buffer);
+    // Init reader
+    sprintf(buffer, "%i | Init reader...", reader);
+    Serial.print(buffer);
 
-        mfrc522[reader].PCD_Init(sdaPins[reader], rstPins[reader]);
+    mfrc522[reader].PCD_Init(sdaPins[reader], RST_PIN);
 
-        Serial.println(" [OK]");
+    Serial.println(" [OK]");
 
-        // Display reader info
-        sprintf(buffer, "%i | ", reader);
-        Serial.print(buffer);
+    // Display reader info
+    sprintf(buffer, "%i | ", reader);
+    Serial.print(buffer);
 
-        mfrc522[reader].PCD_DumpVersionToSerial();
-    }
+    mfrc522[reader].PCD_DumpVersionToSerial();
+  }
 }
-
-int lastint = 0;
 
 //======//
 // LOOP //
 //======//
 void loop() {
-    // Check readers for cards
-    for (uint8_t reader = 0; reader < READER_TOTAL; reader++) {
-
-        if (lastint != reader) {
-            Serial.println(reader);
-            lastint = reader;
-        }
-        
-        // Check for new cards
-        if (!mfrc522[reader].PICC_IsNewCardPresent()) {
-            Serial.println("new card is not present!");
-            return;
-        }
-
-        /*
-        // Select one of the cards
-        if (!mfrc522[reader].PICC_ReadCardSerial()) {
-            return;
-        }
-
-        // Verifing
-        LED_Status(2);
-
-        // Store card UID
-        byte uid[mfrc522[reader].uid.size];
-
-        // Display UID
-        Serial.print(reader);
-        Serial.print(" | ");
-
-        for (int i = 0; i < mfrc522[reader].uid.size; i++) {
-            // Copy the UID
-            uid[i] = mfrc522[reader].uid.uidByte[i]; // MISSING A 0???
-
-            // Display UID
-            Serial.print(uid[i], HEX);
-        }
-        // End display UID
-        Serial.println();
-
-        byte LoadedCards[mfrc522[reader].uid.size][TotalLoadedCards] = {
-            {0x04,0xB3,0xB8,0xBA,0x18,0xD1,0x80},
-            {0x04,0xB3,0xB8,0xBA,0x18,0x31,0x80}
-        };
-
-        // Checking for matches
-        for (int i = 0; i < TotalLoadedCards; i++) {
-            //Serial.print("Checking | ");
-            //Serial.println(i);
-            if ((memcmp(uid, LoadedCards[i], mfrc522[reader].uid.size)) == 0) {
-                //Serial.println("MATCH");
-                // Access Granted
-                LED_Status(1);
-            } else {
-                //Serial.println("NO MATCH");
-            }
-        }
-
-        // Halt PICC
-        mfrc522[reader].PICC_HaltA();
-
-        // Stop encryption on PCD
-        mfrc522[reader].PCD_StopCrypto1();
-
-        // Access Denied
-        LED_Status(3); */
+  // Check readers for cards
+  for (uint8_t reader = 0; reader < READER_TOTAL; reader++) {
+    // Check for new cards
+    if (!mfrc522[reader].PICC_IsNewCardPresent()) {
+      return;
     }
+
+    // Select one of the cards
+    if (!mfrc522[reader].PICC_ReadCardSerial()) {
+      return;
+    }
+
+    // Verifing
+    LED_Status(2);
+
+    // Store card UID
+    byte uid[mfrc522[reader].uid.size];
+
+    // Display UID
+    Serial.print(reader);
+    Serial.print(" | ");
+
+    for (int i = 0; i < mfrc522[reader].uid.size; i++) {
+      // Copy the UID
+      uid[i] = mfrc522[reader].uid.uidByte[i]; // MISSING A 0???
+
+      // Display UID
+      Serial.print(uid[i], HEX);
+    }
+    // End display UID
+    Serial.println();
+
+    byte LoadedCards[mfrc522[reader].uid.size][TotalLoadedCards] = {
+      {0x04,0xB3,0xB8,0xBA,0x18,0xD1,0x80},
+      {0x04,0xB3,0xB8,0xBA,0x18,0x31,0x80}
+    };
+
+    // Checking for matches
+    for (int i = 0; i < TotalLoadedCards; i++) {
+      //Serial.print("Checking | ");
+      //Serial.println(i);
+      if ((memcmp(uid, LoadedCards[i], mfrc522[reader].uid.size)) == 0) {
+        //Serial.println("MATCH");
+        // Access Granted
+        LED_Status(1);
+      } else {
+        //Serial.println("NO MATCH");
+      }
+    }
+
+    // Halt PICC
+    mfrc522[reader].PICC_HaltA();
+
+    // Access Denied
+    LED_Status(3);
+  }
 }
 
 void LED_Status(int status) {
-    switch(status) {
-        case 1: {
-            // Access Granted
-            analogWrite(LED_BLUE_PIN, HIGH);
-            analogWrite(LED_YELLOW_PIN, LOW);
-            analogWrite(LED_RED_PIN, LOW);
-            break;
-        }
-        case 2: {
-            // Verifing
-            analogWrite(LED_BLUE_PIN, LOW);
-            analogWrite(LED_YELLOW_PIN, HIGH);
-            analogWrite(LED_RED_PIN, LOW);
-            break;
-        }
-        case 3: {
-            // Access Denied
-            analogWrite(LED_BLUE_PIN, LOW);
-            analogWrite(LED_YELLOW_PIN, LOW);
-            analogWrite(LED_RED_PIN, HIGH);
-            break;
-        }
+  switch(status) {
+    case 1: {
+      // Access Granted
+      analogWrite(LED_BLUE_PIN, HIGH);
+      analogWrite(LED_YELLOW_PIN, LOW);
+      analogWrite(LED_RED_PIN, LOW);
+      break;
     }
+    case 2: {
+      // Verifing
+      analogWrite(LED_BLUE_PIN, LOW);
+      analogWrite(LED_YELLOW_PIN, HIGH);
+      analogWrite(LED_RED_PIN, LOW);
+      break;
+    }
+    case 3: {
+      // Access Denied
+      analogWrite(LED_BLUE_PIN, LOW);
+      analogWrite(LED_YELLOW_PIN, LOW);
+      analogWrite(LED_RED_PIN, HIGH);
+      break;
+    }
+  }
 }
